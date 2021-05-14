b7c0567e87aff91d4df8409579ea32accb739bf2
Learner045 <shreya.ds045@gmail.com>
2021-05-14 01:25:25 -0400

    changed warnings

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import { Button, Label, Icon } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 import MyPopup from '../util/MyPopup';
 
